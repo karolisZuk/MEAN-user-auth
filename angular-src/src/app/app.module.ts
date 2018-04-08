3d02aad9fa7b9fdf80ddf4bcf7ab9436f8514f76
Karolis <kar.zukas@gmail.com>
2018-04-08 15:59:44 +0300

    routing and navigation

---
+++ angular-src/src/app/app.module.ts
@@ -0,0 +1,45 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ProfileComponent } from './components/profile/profile.component';
+
+const appRoutes: Routes = [
+  {path: '',component: HomeComponent},
+  {path: 'register',component: RegisterComponent},
+  {path: 'login',component: LoginComponent},
+  {path: 'dashboard',component: DashboardComponent},
+  {path: 'profile',component: ProfileComponent}
+]
+
+@NgModule({
+  //modules
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    DashboardComponent,
+    ProfileComponent
+  ],
+  //providers
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  //services
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
